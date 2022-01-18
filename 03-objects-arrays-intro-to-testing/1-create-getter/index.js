ea8d8bc14fd62650605bed2e298d7cc34c3c537b
ysharipbaeva <yana.sharipbaeva@logiqlabs.com>
2022-01-18 21:32:49 +0200

    closure

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,9 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return function (product){
+    let result = {...product};
+    path.split('.').map(item => result && result[item] ? result = result[item] : result = undefined);
+    return result;
+  };
 }
