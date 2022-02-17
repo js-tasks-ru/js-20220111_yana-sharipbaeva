cce9ff5aacec751710cbcf1b7c9d5f4f1df8660c
ysharipbaeva <yana.sharipbaeva@logiqlabs.com>
2022-02-18 00:00:27 +0200

    sortable list stated

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,41 @@
 export default class SortableList {
+  constructor ({ items = [] } = {}) {
+    this.items = items;
+    this.render();
+  }
 
+  render() {
+    this.element = this.getTemplate();
+  }
+
+  initEventListeners () {}
+
+  getTemplate() {
+    const ul = document.createElement('ul');
+    ul.classList.add('sortable-list');
+    this.items.map(item => {
+      item.classList.add('sortable-list__item');
+      ul.append(item);
+    });
+    return ul;
+  }
+
+  getSubElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+  destroy() {
+    this.remove();
+  }
 }
